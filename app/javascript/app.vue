6a4b6ed92919c83094c44f4a83442d89d330c35c
N.Kishino <naopon2009@gmail.com>
2021-04-15 09:42:47 +0900

    ログイン画面途中…

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,5 @@
 <template>
-  
+
 </template>
 
 <script>
@@ -10,7 +10,7 @@ export default {
    },
   data: function () {
     return {
-      message: "Hello Vuuuuuuuee!"
+      
     }
   }
 }
--- app/javascript/login.vue
+++ app/javascript/login.vue
@@ -1,9 +1,16 @@
 <template>
-    
+  <div class="login_form">
+    <div class="login_input">
+      <ul class="login_form_input_li">
+
+
+        <li><input type="text" placeholder="メールアドレス…"/></li>
+        <li><input type="text" placeholder="パスワード…"/></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
