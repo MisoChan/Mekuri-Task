d2ab29c91f11a05b11723817861661ce850b1703
N.Kishino <naopon2009@gmail.com>
2021-04-16 09:23:23 +0900

    ちましま進捗

--- app/javascript/login.vue
+++ app/javascript/login.vue
@@ -2,8 +2,6 @@
   <div class="login_form">
     <div class="login_input">
       <ul class="login_form_input_li">
-
-
         <li><input type="text" placeholder="メールアドレス…"/></li>
         <li><input type="text" placeholder="パスワード…"/></li>
       </ul>
